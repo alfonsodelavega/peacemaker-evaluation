3bee8bfc7d4ec9a55e08e600d492bbb50c42c269
alfonsodelavega <alfonso.delavega@tuta.io>
2021-03-15 22:09:42 +0000

    Add jvm args and run gc

--- org.eclipse.epsilon.peacemaker.benchmarks/run_benchmarks.sh
+++ org.eclipse.epsilon.peacemaker.benchmarks/run_benchmarks.sh
@@ -4,8 +4,8 @@
 # f   - number of forks (times each benchmark, with iterations and warmups, is repeated)
 # i   - number of iterations
 # wi  - number of warmup iterations
-# gc  - force garbage collection between iterations (currently: false)
+# gc  - force garbage collection between iterations
 # o   - output file (defaults to jhm-result.csv)
 # rf  - results format (csv, json, anything else?)
 
-java -jar target/benchmarks.jar -foe true -bm avgt -tu ms -f 1 -i 5 -wi 3 -rf csv
+java -jar target/benchmarks.jar -foe true -bm avgt -tu ms -f 1 -i 5 -wi 3 -rf csv -jvmArgs "-Xms1G -Xmx12G" -gc true

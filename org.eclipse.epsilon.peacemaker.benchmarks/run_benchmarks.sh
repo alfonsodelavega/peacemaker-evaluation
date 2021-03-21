8881168b356fbbde7c69c5c542da5098e7509468
alfonsodelavega <alfonso.delavega@tuta.io>
2021-03-21 17:08:18 +0000

    Update benchmarks startup parameters

--- org.eclipse.epsilon.peacemaker.benchmarks/run_benchmarks.sh
+++ org.eclipse.epsilon.peacemaker.benchmarks/run_benchmarks.sh
@@ -4,8 +4,10 @@
 # f   - number of forks (times each benchmark, with iterations and warmups, is repeated)
 # i   - number of iterations
 # wi  - number of warmup iterations
+# r   - minimum seconds for iteration (repeats benchmark call if not yet reached, then averages)
+# w   - minimum seconds for warmup iteration
 # gc  - force garbage collection between iterations
 # o   - output file (defaults to jhm-result.csv)
 # rf  - results format (csv, json, anything else?)
 
-java -jar target/benchmarks.jar -foe true -bm avgt -tu ms -f 1 -i 5 -wi 3 -rf csv -jvmArgs "-Xms1G -Xmx12G" -gc true
+java -jar target/benchmarks.jar -foe true -bm avgt -tu ms -f 1 -i 10 -wi 5 -r 2 -w 2 -rf csv
